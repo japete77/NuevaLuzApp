0d17012711ae84dda8a5a5de85c70389a2c04abc
Juan Pablo Gonzalez Jara <juanpablogonzalezjara@MacBook-Pro-de-Juan.local>
2020-01-06 21:50:51 +0100

    Remove search in titles by author

--- src/pages/titlesbyauthor/titlesbyauthor.ts
+++ src/pages/titlesbyauthor/titlesbyauthor.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { AudioBooksProvider } from 'src/providers/audiobooks/audiobooks';
 import { Title } from 'src/models/title';
 import { Router, ActivatedRoute } from '@angular/router';
-import { TitleResult } from 'src/models/titleresult';
 
 @Component({
     selector: 'page-titlebyauthor',
@@ -17,7 +16,6 @@ export class TitlesByAuthorPage implements OnInit {
 
     private pageSize = 25;
     private index = 0;
-    private filterText = '';
 
     constructor(
         private router: Router,
@@ -41,48 +39,6 @@ export class TitlesByAuthorPage implements OnInit {
             this.titles.push(element);
         });
     }
-
-    // filter(event: any) {
-
-    //   this.filterText = event.target.value;
-
-    //   this.loading = true;
-    //   this.index = 0;
-    //   this.titles = [];
-
-    //   if (this.filterText !== undefined && this.filterText !== '')
-    //   {
-
-    //     this.audiobooks.SearchBooksByTitle(event.target.value, this.index, this.pageSize)
-    //     .subscribe(value => {
-
-    //       this.index += this.pageSize;
-    //       this.hasMore = (this.index < value.SearchTitlesResult.Total);
-
-    //       value.SearchTitlesResult.Titles.forEach(element => {
-    //         this.titles.push(element);
-    //       });
-
-    //       this.loading = false;
-    //     });
-
-    //   } else {
-
-    //     this.audiobooks.GetBooksByTitle(this.index, this.pageSize)
-    //     .subscribe(value => {
-
-    //       this.index += this.pageSize;
-    //       this.hasMore = (this.index < value.GetTitlesResult.Total);
-
-    //       value.GetTitlesResult.Titles.forEach(element => {
-    //         this.titles.push(element);
-    //       });
-
-    //       this.loading = false;
-    //     });
-
-    //   }
-    // }
 
     gotoDetails(id: string) {
         this.router.navigateByUrl(`bookdetails/${id}`);
